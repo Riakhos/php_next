c2537c86ed91170563633a999ebae993690e05ff
Richard Bonnegent <richard.bonnegent.com>
2024-09-09 17:56:06 +0200

    pdo_next

--- commentaire/index.php
+++ commentaire/index.php
@@ -6,7 +6,7 @@
 <?php
 
 // Je me connecte à la base de données
-$pdo = new PDO('mysql:host=localhost;dbname=blog_comments','root', 'jetyej', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$pdo = new PDO('mysql:host=localhost;dbname=blog_comments','root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 // todo 2. Connexion à la BDD
 //* var_dump($pdo);
